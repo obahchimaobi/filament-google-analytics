bd34ffd2caa373f47fdad4ff535afc839adb8874
obahchimaobi <anthonyobah37@gmail.com>
2025-08-24 09:12:28 +0100

    Rename package and enhance authorization handling in GoogleAnalyticsPlugin
    Renames package and enhances authorization
    
    Renames the package to reflect the new author.
    
    Adds authorization functionality to the GoogleAnalyticsPlugin,
    allowing developers to control access to the dashboard.

--- src/GoogleAnalyticsPlugin.php
+++ src/GoogleAnalyticsPlugin.php
@@ -4,15 +4,33 @@ declare(strict_types=1);
 
 namespace BezhanSalleh\GoogleAnalytics;
 
+use Closure;
 use BezhanSalleh\GoogleAnalytics\Pages\GoogleAnalyticsDashboard;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
+use Filament\Support\Concerns\EvaluatesClosures;
 
 class GoogleAnalyticsPlugin implements Plugin
 {
+    use EvaluatesClosures;
+
+    protected bool | Closure $authorizeUsing = true;
+
     public static function make(): static
     {
         return app(static::class);
+    }
+
+    public function authorize(bool | Closure $callback = true): static
+    {
+        $this->authorizeUsing = $callback;
+
+        return $this;
+    }
+
+    public function isAuthorized(): bool
+    {
+        return $this->evaluate($this->authorizeUsing) === true;
     }
 
     public function getId(): string
